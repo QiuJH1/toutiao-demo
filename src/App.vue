36ad7ac579e8043b128f27073d4fa1d90a10d028
QJH <1006888645@qq.com>
2022-04-20 20:07:30 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <!--  尝试使用icon图标和vant
+      <i class="toutiao toutiao-dianzan"></i>
+    <i class="toutiao toutiao-yanzhengma">  </i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group> -->
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 引入全局样式
+// 配置按需引入后，将不允许直接导入所有组件。这时到babel.config.js里把plugins的代码删除，就能进行导入所有组件
+//  加载Vant核心组件库
+import Vant from 'vant'
+//  加载Vant全局样式
+import 'vant/lib/index.css'
+// 引入less全局样式
 import './styles/index.less'
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
